f010b5cfe5cf664ae72878e96a35f27fa7ebe06e
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2019-01-09 16:28:27 +0530

    created migration files for GE software

--- database/migrations/2019_01_09_082614_create_contracts_table.php
+++ database/migrations/2019_01_09_082614_create_contracts_table.php
@@ -14,8 +14,12 @@ class CreateContractsTable extends Migration
     public function up()
     {
         Schema::create('contracts', function (Blueprint $table) {
-
+            
             $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->integer('no_of_jobs');
+            $table->integer('no_of_phases');
             $table->string('contract_type');
             $table->string('contact_person');
             $table->string('contract_location');
@@ -23,6 +27,10 @@ class CreateContractsTable extends Migration
             $table->string('contract_city');
             $table->string('contract_payment_terms');
             $table->string('contract_renewal_terms');
+            $table->integer('contract_cost');
+            $table->text('form_16');
+            $table->text('form_B');
+            $table->text('terms_conditions');
             $table->string('contact_number');
             $table->string('area_sq_feet');
             $table->string('premises');
